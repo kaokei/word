34df305eda4da633ae4d2e6c64f757cf927c6a32
kaokei <kaokei.rst@gmail.com>
2024-12-26 20:40:20 +0800

    update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@ useSeoMeta({
 
 <template>
   <div class="prose ps-6 pt-10 lg:prose-xl">
-    <h1>有趣的网页002</h1>
+    <h1>有趣的网页001</h1>
     <p>积累一些有趣的网页。纯前端实现，不依赖后端。不需要登录。</p>
     <p>
       <NuxtLink class="link-hover link" to="/p/malicious"
