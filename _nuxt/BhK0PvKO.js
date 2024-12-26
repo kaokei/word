a0f65ddb3478b37022c95a5479b99230d7f8bcf0
kaokei <kaokei.rst@gmail.com>
2024-12-27 00:22:13 +0800

    Deploying to gh-pages from @ kaokei/word@789e84988dbbb535e0331069c486dac0a9d0e681 🚀

---
+++ _nuxt/BhK0PvKO.js
@@ -0,0 +1,1 @@
+import{u as t}from"./YVioybMU.js";import{h as s,c as a,a as e,o as n}from"./8xmYlyqM.js";import"./BfSUcfZx.js";const p={class:"prose ps-6 pt-10 lg:prose-xl"},c=s({__name:"index",setup(r){return t({title:"kaokei"}),(l,o)=>(n(),a("div",p,o[0]||(o[0]=[e("h1",null,"高效背单词",-1),e("p",null,"专门用于背单词的网站。",-1),e("p",null,"背一个单词就是赚一个单词",-1)])))}});export{c as default};
---
+++ _nuxt/BnGeeXjX.js
@@ -0,0 +1,1 @@
+import{u as s}from"./YVioybMU.js";import{h as t,c as l,a as e,o as a}from"./8xmYlyqM.js";import"./BfSUcfZx.js";const n={class:"px-6 py-10"},u=t({__name:"index",setup(r){return s({title:"kaokei"}),(p,o)=>(a(),l("div",n,o[0]||(o[0]=[e("article",{class:"prose mb-4 lg:prose-xl"},[e("h2",null,"CSS格式化"),e("p",null,"好好学习，天天向上")],-1),e("div",null,null,-1)])))}});export{u as default};
